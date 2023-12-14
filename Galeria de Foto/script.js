a292bb6b84f8933988efc37a9d4fdd216682398f
JULIO SENA <gm.sena@hotmail.com>
2023-12-14 20:46:02 -0300

    Primeira galeria de fotos

---
+++ Galeria de Foto/script.js
@@ -0,0 +1,23 @@
+
+/*
+
+const carrossel = document.querySelector("#carrossel");
+    let index = 0;
+
+    function next() {
+      index = (index + 1) % carrossel.children.length;
+      carrossel.children[index].classList.add("ativo");
+      carrossel.children[index - 1].classList.remove("ativo");
+    }
+
+    function previous() {
+      index = (index - 1 + carrossel.children.length) % carrossel.children.length;
+      carrossel.children[index].classList.add("ativo");
+      carrossel.children[index + 1].classList.remove("ativo");
+    }
+
+    setInterval(next, 2000);
+
+    document.querySelector("#carrossel .botao-proximo").addEventListener("click", next);
+    document.querySelector("#carrossel .botao-anterior").addEventListener("click", previous);
+    */
